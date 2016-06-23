3410f5e9a13f75886b80e23d4ae9b627c63ed05a
Tanya <tanya.brassie@utexas.edu>
2016-06-23 14:45:07 -0400

    updating things lala

--- mobile-home/scripts/mobile-home.js
+++ mobile-home/scripts/mobile-home.js
@@ -28,12 +28,83 @@ var $mobileHome = $($(".body-elements")[0]);
 
 $(window).on("scroll", function(e) {
 	if ($(this).scrollTop() > 198) {
-		$mobileHomeContainer.css( {"position": "fixed", "top": 0, "padding": "20px 0", "width": "85%"});
+		$mobileHomeContainer.css( {"position": "fixed", "top": 0, "padding": "20px 0", "width": "85%", "border-bottom": "2px solid black"});
 
 	} else {
-		$mobileHomeContainer.css({"position": "initial", "padding-bottom": "0px", "width": "inherit"});
+		$mobileHomeContainer.css({"position": "initial", "padding-bottom": "0px", "width": "inherit", "border-bottom": "0px solid black"});
 	}
 });
+
+
+var topSwatchChoices = {
+	
+	yellow: {
+		name: "topstripe",
+		swatchName: "yellow",
+		"swatch-class": "body-stripe-top",
+		value: "#d3d400",
+		id: "top-color-a"
+	},	
+	seafoam: {
+		name: "topstripe",
+		swatchName: "seafoam",
+		"swatch-class": "body-stripe-top",
+		value: "#aafffd",
+		id: "top-color-b"
+	},
+	teal: {
+		name: "topstripe",
+		swatchName: "teal",
+		"swatch-class": "body-stripe-top",
+		value: "#09dfff",
+		id: "top-color-c"
+	},
+	red: {
+		name: "topstripe",
+		swatchName: "red",
+		"swatch-class": "body-stripe-top",
+		value: "#ff000",
+		id: "top-color-d"
+	}, 
+	pink: {
+		name: "topstripe",
+		swatchName: "pink",
+		"swatch-class": "body-stripe-top",
+		value: "#ffc6ae",
+		id: "top-color-e"
+	}, 
+	black: {
+		name: "topstripe",
+		swatchName: "black",
+		"swatch-class": "body-stripe-top",
+		value: "#000",
+		id: "top-color-f"
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ mobile-home/scripts/swatch-data.js
@@ -0,0 +1,193 @@
+//ALL OF MY SWATCH CHOICES 
+
+var topSwatchChoices = {
+	
+	yellow: {
+		name: "topstripe",
+		swatchName: "yellow",
+		"swatch-class": "body-stripe-top",
+		value: "#d3d400",
+		id: "top-color-a"
+	},	
+	seafoam: {
+		name: "topstripe",
+		swatchName: "seafoam",
+		"swatch-class": "body-stripe-top",
+		value: "#aafffd",
+		id: "top-color-b"
+	},
+	teal: {
+		name: "topstripe",
+		swatchName: "teal",
+		"swatch-class": "body-stripe-top",
+		value: "#09dfff",
+		id: "top-color-c"
+	},
+	red: {
+		name: "topstripe",
+		swatchName: "red",
+		"swatch-class": "body-stripe-top",
+		value: "#ff000",
+		id: "top-color-d"
+	}, 
+	pink: {
+		name: "topstripe",
+		swatchName: "pink",
+		"swatch-class": "body-stripe-top",
+		value: "#ffc6ae",
+		id: "top-color-e"
+	}, 
+	black: {
+		name: "topstripe",
+		swatchName: "black",
+		"swatch-class": "body-stripe-top",
+		value: "#000",
+		id: "top-color-f"
+	}
+};
+
+
+var bodySwatchChoices = {
+	
+	yellow: {
+		name: "bodystripe",
+		swatchName: "yellow",
+		"swatch-class": "body-stripe",
+		value: "#d3d400",
+		id: "color-a"
+	},	
+	seafoam: {
+		name: "bodystripe",
+		swatchName: "seafoam",
+		"swatch-class": "body-stripe",
+		value: "#aafffd",
+		id: "color-b"
+	},
+	teal: {
+		name: "bodystripe",
+		swatchName: "teal",
+		"swatch-class": "body-stripe",
+		value: "#09dfff",
+		id: "color-c"
+	},
+	red: {
+		name: "bodystripe",
+		swatchName: "red",
+		"swatch-class": "body-stripe",
+		value: "#ff000",
+		id: "color-d"
+	}, 
+	pink: {
+		name: "bodystripe",
+		swatchName: "pink",
+		"swatch-class": "body-stripe",
+		value: "#ffc6ae",
+		id: "color-e"
+	}, 
+	black: {
+		name: "bodystripe",
+		swatchName: "black",
+		"swatch-class": "body-stripe",
+		value: "#000",
+		id: "color-f"
+	}
+};
+
+
+var bottomStripeSwatchChoices = {
+	
+	yellow: {
+		name: "bodystripebottom",
+		swatchName: "yellow",
+		"swatch-class": "body-stripe-bottom",
+		value: "#d3d400",
+		id: "bottom-color-a"
+	},	
+	seafoam: {
+		name: "bodystripebottom",
+		swatchName: "seafoam",
+		"swatch-class": "body-stripe-bottom",
+		value: "#aafffd",
+		id: "bottom-color-b"
+	},
+	teal: {
+		name: "bodystripebottom",
+		swatchName: "teal",
+		"swatch-class": "body-stripe-bottom",
+		value: "#09dfff",
+		id: "bottom-color-c"
+	},
+	red: {
+		name: "bodystripebottom",
+		swatchName: "red",
+		"swatch-class": "body-stripe-bottom",
+		value: "#ff000",
+		id: "bottom-color-d"
+	}, 
+	pink: {
+		name: "bodystripebottom",
+		swatchName: "pink",
+		"swatch-class": "body-stripe-bottom",
+		value: "#ffc6ae",
+		id: "bottom-color-e"
+	}, 
+	black: {
+		name: "bodystripebottom",
+		swatchName: "black",
+		"swatch-class": "body-stripe-bottom",
+		value: "#000",
+		id: "bottom-color-f"
+	}
+};
+
+
+var doorSwatchChoices = {
+	
+	yellow: {
+		name: "doorcolor",
+		swatchName: "yellow",
+		"swatch-class": "door",
+		value: "#d3d400",
+		id: "door-color-a"
+	},	
+	seafoam: {
+		name: "doorcolor",
+		swatchName: "seafoam",
+		"swatch-class": "door",
+		value: "#aafffd",
+		id: "door-color-b"
+	},
+	teal: {
+		name: "doorcolor",
+		swatchName: "teal",
+		"swatch-class": "door",
+		value: "#09dfff",
+		id: "door-color-c"
+	},
+	red: {
+		name: "doorcolor",
+		swatchName: "red",
+		"swatch-class": "door",
+		value: "#ff000",
+		id: "door-color-d"
+	}, 
+	pink: {
+		name: "doorcolor",
+		swatchName: "pink",
+		"swatch-class": "door",
+		value: "#ffc6ae",
+		id: "door-color-e"
+	}, 
+	black: {
+		name: "doorcolor",
+		swatchName: "black",
+		"swatch-class": "door",
+		value: "#000",
+		id: "door-color-f"
+	}
+};
+
+
+
+ 
+
