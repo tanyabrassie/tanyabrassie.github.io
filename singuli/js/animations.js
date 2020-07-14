f07b465a169e845db1c1d348f77dc0e31ad7cf08
Tanya Brassie <tanyabrassie@TANYAS-COMPUTING-MACHINE-2.local>
2020-07-14 14:40:05 -0600

    no-images test

---
+++ singuli/js/animations.js
@@ -0,0 +1,10 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('container'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: perfume,
+});
+
+console.log(animation);
+console.log('hello again');
