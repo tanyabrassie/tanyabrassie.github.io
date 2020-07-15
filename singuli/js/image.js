020a6df1f2a12b0531792009b2a7ae8c31b23efe
Tanya Brassie <tanyabrassie@TANYAS-COMPUTING-MACHINE-2.local>
2020-07-15 13:37:43 -0600

    sunglasses

--- singuli/js/image.js
+++ singuli/js/image.js
@@ -5,3 +5,11 @@ var imageAnimation = lottie.loadAnimation({
   autoplay: true,
   animationData: perfume,
 });
+
+var sunglassesAnimation = lottie.loadAnimation({
+  container: document.getElementsByClassName('sunglasses__animation-image')[0],
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: sunglasses,
+});
