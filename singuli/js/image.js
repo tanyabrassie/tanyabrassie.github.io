dbbbd1431929aec4e9f2924f064dacf7fddfc3cf
Tanya Brassie <tanyabrassie@TANYAS-COMPUTING-MACHINE-2.local>
2020-07-15 12:25:07 -0600

    test

---
+++ singuli/js/image.js
@@ -0,0 +1,7 @@
+var imageAnimation = lottie.loadAnimation({
+  container: document.getElementsByClassName('perfume__animation-image')[0],
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: perfume,
+});
---
+++ singuli/js/no-image.js
@@ -0,0 +1,8 @@
+var noImageAnimation = lottie.loadAnimation({
+  container: document.getElementsByClassName('perfume__animation')[0],
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: perfumeNoImage,
+});
+
