199b08a9cf5d15e07264b7e92261e6377ebc5aec
Tanya Brassie <tanyabrassie@tanyascmachine2.home>
2020-07-21 15:18:10 -0400

    adding hero image

--- singuli/js/animations.js
+++ singuli/js/animations.js
@@ -13,3 +13,61 @@ var sunglassesAnimation = lottie.loadAnimation({
   autoplay: true,
   animationData: sunglasses,
 });
+
+var heroShoeBackAnimation = lottie.loadAnimation({
+  container: document.getElementsByClassName('hero__animation__back')[0],
+  renderer: 'svg',
+  loop: false,
+  autoplay: false,
+  animationData: heroShoeBack,
+});
+
+var heroShoeMidAnimation = lottie.loadAnimation({
+  container: document.getElementsByClassName('hero__animation__mid')[0],
+  renderer: 'svg',
+  loop: false,
+  autoplay: false,
+  animationData: heroShoeMid,
+});
+
+var heroShoeFrontAnimation = lottie.loadAnimation({
+  container: document.getElementsByClassName('hero__animation__front')[0],
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  animationData: heroShoeFront,
+});
+
+setTimeout(() => {
+  heroShoeFrontAnimation.play();
+}, 100);
+
+setTimeout(() => {
+  heroShoeMidAnimation.play();
+}, 200);
+
+setTimeout(() => {
+  heroShoeBackAnimation.play();
+}, 210);
+
+
+function isScrolledIntoView(el) {
+  var rect = el.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+  console.log('hi');
+
+  // Only completely visible elements return true:
+  var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+  console.log('isVisible', isVisible);
+  // Partially visible elements return true:
+  //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+  return isVisible;
+};
+
+console.log(document);
+const perfumeSection = document.getElementById('perfume-section');
+console.log(perfumeSection);
+isScrolledIntoView(perfumeSection);
+
+
