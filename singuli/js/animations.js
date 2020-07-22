00db452a59c60819a2ee1dadad29a65210b4d5f1
Tanya Brassie <tanyabrassie@tanyascmachine2.home>
2020-07-22 08:42:28 -0400

    Canvas test

--- singuli/js/animations.js
+++ singuli/js/animations.js
@@ -1,73 +1,141 @@
-var perfumeAnimation = lottie.loadAnimation({
-  container: document.getElementsByClassName('perfume__animation')[0],
-  renderer: 'svg',
+lottie.setQuality(2);
+
+const perfumeAnimationDiv = document.getElementById('lottie-perfume');
+const sunglassesAnimationDiv = document.getElementById('lottie-sunglasses');
+const teeAnimationDiv = document.getElementById('lottie-tee');
+const watchAnimationDiv = document.getElementById('lottie-watch'); 
+
+const shoeBackDiv = document.getElementById('animation-back-shoe');
+const shoeMidDiv = document.getElementById('animation-mid-shoe');
+const shoeFrontDiv = document.getElementById('animation-front-shoe');
+
+const animationDivs = [perfumeAnimationDiv, sunglassesAnimationDiv, teeAnimationDiv, watchAnimationDiv]; 
+
+const teeAnimation = lottie.loadAnimation({
+  container: teeAnimationDiv,
+  renderer: 'canvas',
   loop: true,
-  autoplay: true,
+  autoplay: false,
+  animationData: tee,
+});
+
+const perfumeAnimation = lottie.loadAnimation({
+  container: perfumeAnimationDiv,
+  renderer: 'canvas',
+  loop: true,
+  autoplay: false,
   animationData: perfume,
 });
 
-var sunglassesAnimation = lottie.loadAnimation({
-  container: document.getElementsByClassName('sunglasses__animation')[0],
-  renderer: 'svg',
+const watchAnimation = lottie.loadAnimation({
+  container: watchAnimationDiv,
+  renderer: 'canvas',
+  loop: true,
+  autoplay: false,
+  animationData: watch,
+  rendererSettings: {
+    // context: canvasContext, // the canvas context
+    preserveAspectRatio: 'none', // Supports the same options as the svg element's preserveAspectRatio property
+    clearCanvas: false,
+    progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
+    hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
+    className: 'some-css-class-name',
+    id: 'some-id',
+  },
+});
+
+const sunglassesAnimation = lottie.loadAnimation({
+  container: sunglassesAnimationDiv,
+  renderer: 'canvas',
   loop: true,
   autoplay: true,
   animationData: sunglasses,
 });
 
-var heroShoeBackAnimation = lottie.loadAnimation({
-  container: document.getElementsByClassName('hero__animation__back')[0],
-  renderer: 'svg',
+const shoeBackAnimation = lottie.loadAnimation({
+  container: shoeBackDiv,
+  renderer: 'canvas',
   loop: false,
   autoplay: false,
   animationData: heroShoeBack,
 });
 
-var heroShoeMidAnimation = lottie.loadAnimation({
-  container: document.getElementsByClassName('hero__animation__mid')[0],
-  renderer: 'svg',
+const shoeMidAnimation = lottie.loadAnimation({
+  container: shoeMidDiv,
+  renderer: 'canvas',
   loop: false,
   autoplay: false,
   animationData: heroShoeMid,
 });
 
-var heroShoeFrontAnimation = lottie.loadAnimation({
-  container: document.getElementsByClassName('hero__animation__front')[0],
-  renderer: 'svg',
+const shoeFrontAnimation = lottie.loadAnimation({
+  container: shoeFrontDiv,
+  renderer: 'canvas',
   loop: false,
-  autoplay: true,
+  autoplay: false,
   animationData: heroShoeFront,
 });
 
-setTimeout(() => {
-  heroShoeFrontAnimation.play();
-}, 100);
+shoeFrontAnimation.addEventListener('loaded_images', () => {
+  console.log('im loading the images!');
 
-setTimeout(() => {
-  heroShoeMidAnimation.play();
-}, 200);
+  shoeFrontAnimation.play();
+  shoeMidAnimation.play();
+  shoeBackAnimation.play();
+});
 
-setTimeout(() => {
-  heroShoeBackAnimation.play();
-}, 210);
+// shoeFrontAnimation.addEventListener('onComplete', () => {
+//   console.log('im complete!');
+// });
+
+// shoeFrontAnimation.onLoopComplete = function() {
+//   console.log('completed now?');
+// };
 
 
-function isScrolledIntoView(el) {
-  var rect = el.getBoundingClientRect();
-  var elemTop = rect.top;
-  var elemBottom = rect.bottom;
-  console.log('hi');
+// setTimeout(() => {
+//   shoeFrontAnimation.play();
+// }, 10);
 
-  // Only completely visible elements return true:
-  var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-  console.log('isVisible', isVisible);
-  // Partially visible elements return true:
-  //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-  return isVisible;
+// setTimeout(() => {
+//   shoeMidAnimation.play();
+// }, 200);
+
+// setTimeout(() => {
+//   shoeBackAnimation.play();
+// }, 210);
+
+const animationDict = {
+  'lottie-perfume' : perfumeAnimation,
+  'lottie-sunglasses' : sunglassesAnimation,
+  'lottie-tee' : teeAnimation,
+  'lottie-watch' : watchAnimation,
 };
 
-console.log(document);
-const perfumeSection = document.getElementById('perfume-section');
-console.log(perfumeSection);
-isScrolledIntoView(perfumeSection);
 
-
+(function() {
+  let windowHeight;
+  function init() {
+    windowHeight = window.innerHeight;
+  }
+  function checkPosition() {
+    for (var i = 0; i < animationDivs.length; i++) {
+      var animationDiv = animationDivs[i];
+      console.log(animationDiv);
+      console.log('hi');
+      var positionFromTop = animationDiv.getBoundingClientRect().top;
+      var height = animationDiv.getBoundingClientRect().height;
+      const animation = animationDict[animationDiv.id];
+      
+      if (positionFromTop - windowHeight <= 0 && positionFromTop + height >=0) {
+        animation.play();
+      } else {
+        animation.stop();
+      }
+    }
+  }
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+  init();
+  checkPosition();
+})();
