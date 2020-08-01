3e792f68141ee0695ff95726eb66309177b6ee28
Tanya Brassie <>
2020-07-31 20:33:54 -0400

    Updating to svg, removing scroll handler

--- singuli/js/mobileAnimations.js
+++ singuli/js/mobileAnimations.js
@@ -44,58 +44,108 @@ const mobileAnimationData = {
   'lottie-watch-mobile' : mobileWatch,
 };
 
-function checkMobilePositions(windowHeight) {
-  for (var i = 0; i < mobileAnimationDivs.length; i++) {
+const watchAnimationMobile = lottie.loadAnimation({
+  container: watchAnimationDivMobile,
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: mobileWatch,
+  rendererSettings: {
+    progressiveLoad: false,
+    className: 'svg-test'
+  },
+});
 
-    // go through each animation div and find its size
-    var animationDiv = mobileAnimationDivs[i];
-    var positionFromTop = animationDiv.getBoundingClientRect().top;
-    var height = animationDiv.getBoundingClientRect().height;
+const teeAnimationMobile = lottie.loadAnimation({
+  container: teeAnimationDivMobile,
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: mobileTee,
+  rendererSettings: {
+    progressiveLoad: false,
+    className: 'svg-test'
+  },
+});
 
-    // IF ANIMATION IS IN VIEW
-    if (positionFromTop - windowHeight <= 0 && positionFromTop + height >=0) {      
+const sunglassesAnimationMobile = lottie.loadAnimation({
+  container: sunglassesAnimationDivMobile,
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: mobileSunglasses,
+  rendererSettings: {
+    progressiveLoad: false,
+    className: 'svg-test'
+  }
+});
+
+const perfumeAnimationMobile = lottie.loadAnimation({
+  container: perfumeAnimationDivMobile,
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: mobilePerfume,
+  rendererSettings: {
+    progressiveLoad: false,
+    className: 'svg-test'
+  }
+});
+
+
+// function checkMobilePositions(windowHeight) {
+//   for (var i = 0; i < mobileAnimationDivs.length; i++) {
+
+//     // go through each animation div and find its size
+//     var animationDiv = mobileAnimationDivs[i];
+//     var positionFromTop = animationDiv.getBoundingClientRect().top;
+//     var height = animationDiv.getBoundingClientRect().height;
+
+//     // IF ANIMATION IS IN VIEW
+//     if (positionFromTop - windowHeight <= 0 && positionFromTop + height >=0) {      
       
-      // IF ANIMATION IS NOT NULL 
-      if (!mobileAnimations[animationDiv.id]) {
-        console.log('creating animation');
-        const animation = lottie.loadAnimation({
-          renderer: 'canvas',
-          name: [animationDiv.id],
-          loop: false,
-          autoplay: false,
-          animationData: mobileAnimationData[[animationDiv.id]],
-          rendererSettings: {
-          context: mobileCanvases[animationDiv.id].getContext('2d'),
-          preserveAspectRatio: scaleModes[animationDiv.id],
-          },
-        });
+//       // IF ANIMATION IS NOT NULL 
+//       if (!mobileAnimations[animationDiv.id]) {
+//         console.log('creating animation');
+//         const animation = lottie.loadAnimation({
+
+//           renderer: 'svg',
+//           name: [animationDiv.id],
+//           loop: false,
+//           autoplay: false,
+//           animationData: mobileAnimationData[[animationDiv.id]],
+//           rendererSettings: {
+//           context: mobileCanvases[animationDiv.id].getContext('2d'),
+//           preserveAspectRatio: scaleModes[animationDiv.id],
+//           },
+//         });
         
-        mobileAnimations[animationDiv.id] = animation;
+//         mobileAnimations[animationDiv.id] = animation;
   
-        // update animation dict with animation
-        animation.play();
-      } 
-    } else {
-      if (!!mobileAnimations[animationDiv.id]) {
-        lottie.destroy([animationDiv.id]);
-        mobileAnimations[animationDiv.id] = null;
-      }
-    }
-  }
-}
+//         // update animation dict with animation
+//         animation.play();
+//       } 
+//     } else {
+//       if (!!mobileAnimations[animationDiv.id]) {
+//         lottie.destroy([animationDiv.id]);
+//         mobileAnimations[animationDiv.id] = null;
+//       }
+//     }
+//   }
+// }
 
-(function() {
-  let windowHeight;
-  function init() {
-    windowHeight = window.innerHeight;
-  }
+// (function() {
+//   let windowHeight;
+//   function init() {
+//     windowHeight = window.innerHeight;
+//   }
   
-  function checkElementPositions() {
-    checkMobilePositions(windowHeight);
-    // fadeInTextMobile(windowHeight);
-  }
+//   function checkElementPositions() {
+//     checkMobilePositions(windowHeight);
+//     // fadeInTextMobile(windowHeight);
+//   }
 
-  window.addEventListener('scroll', checkElementPositions);
-  init();
-  checkElementPositions();
-})();
+//   window.addEventListener('scroll', checkElementPositions);
+//   init();
+//   checkElementPositions();
+// })();
